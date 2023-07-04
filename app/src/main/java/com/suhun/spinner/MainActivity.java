f0656e6e51c1e4c678ee388e3ccccb34835e9f20
suhunlin <lin.suhun@msa.hinet.net>
2023-07-04 12:41:14 +0800

    Add Spinner items setting

--- app/src/main/java/com/suhun/spinner/MainActivity.java
+++ app/src/main/java/com/suhun/spinner/MainActivity.java
@@ -3,12 +3,38 @@ package com.suhun.spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner spinner;
+    private ArrayAdapter<String> arrayAdapter;
+    private String[] items = {"item1", "item2", "item3", "item4", "item5"};
+    private String tag = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initSpinner();
+    }
+    private void initSpinner(){
+        spinner = findViewById(R.id.mySpinner);
+        arrayAdapter = new ArrayAdapter<>(this, androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item, items);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(tag, "+++++position:"+position+"id:"+id+"使用者選擇："+items[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
