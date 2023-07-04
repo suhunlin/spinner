8bd0658b9f21c0bd646ea93fe547bde0f5b245f4
suhunlin <lin.suhun@msa.hinet.net>
2023-07-04 17:12:51 +0800

    Add Spinner DatePickerDialog setting

--- app/src/main/java/com/suhun/spinner/MainActivity.java
+++ app/src/main/java/com/suhun/spinner/MainActivity.java
@@ -2,24 +2,31 @@ package com.suhun.spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
     private Spinner spinner;
     private ArrayAdapter<String> arrayAdapter;
     private String[] items = {"item1", "item2", "item3", "item4", "item5"};
     private String tag = MainActivity.class.getSimpleName();
+    private TextView date, time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initSpinner();
+        date = findViewById(R.id.dateSetting);
     }
     private void initSpinner(){
         spinner = findViewById(R.id.mySpinner);
@@ -37,4 +44,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void dateSettingFun(View view){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, DatePickerDialog.THEME_DEVICE_DEFAULT_DARK,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        date.setText(year+"/"+(month+1)+"/"+dayOfMonth);
+                    }
+                }, 2023, 8, 4);
+        datePickerDialog.show();
+
+    }
+
 }
